d48b124c83ee8409920dbdecb891e0bd9d4f0683
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-09-20 17:18:27 +0330

    Create Recipe APIs

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,10 @@
 """
 Test for models.
 """
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
+from core import models
 
 class TestModel(TestCase):
     """Test models."""
@@ -44,3 +45,19 @@ class TestModel(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+    
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description,'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
---
+++ app/recipe/serializers.py
@@ -0,0 +1,16 @@
+"""
+Serializers for recipe apis.
+"""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializers(serializers.ModelSerializer):
+    """Serializer for recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
