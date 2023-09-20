35a7510de9f1fa058f1f93687d6831189c8c3bc1
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-09-20 17:30:04 +0330

    Create Recipe APIs

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -14,3 +14,9 @@ class RecipeSerializers(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,11 +9,9 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Recipe
 from recipe import serializers
 
-
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    
-    serializer_class = serializers.RecipeSerializer    
+    serializer_class = serializers.RecipeDetailSerializer   
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -21,3 +19,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+    
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+    
+    def perform_create(self, serializer):
+        """Create a new user."""
+        serializer.save(user=self.request.user)
