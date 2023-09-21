e90f5c390055f6a792651ce3031f96a346553112
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-09-21 16:26:51 +0330

    Serializers for Tags and Tests

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -14,7 +14,7 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
-router.register('tags', views.TagsViewSet)
+router.register('tags', views.TagViewSet)
 
 app_name = 'recipe'
 
